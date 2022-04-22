658e99bbe92bafbf42e0b28f503a65c139a91f82
JesusOrtega <jesusortegadc@gmail.com>
2022-04-21 23:25:42 -0700

    added new dependency and @PostConstruct annotation

--- src/main/java/entity/Parrot.java
+++ src/main/java/entity/Parrot.java
@@ -2,6 +2,8 @@ package entity;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 // By using the @Component annotation over the class, we instruct Spring to create
 // an instance of this class and add it to its context.
 @Component
@@ -12,6 +14,11 @@ public class Parrot {
     private String name;
 
     public Parrot(){ }
+
+    @PostConstruct
+    public void init(){
+        this.name = "Giuseppe";
+    }
 
     public String getName(){
         return name;
