7b2950ccf53a616db00187c4d83f7497062f465a
Jesus Ortega del Castillo <52591802+1JesusOrtegadelCastillo@users.noreply.github.com>
2022-04-21 22:05:31 -0700

    Update ProjectConfig.java

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 // the configuration annotation helps spring to understand
 // that his class is a configuration class.
 @Configuration
+//With @ComponentScan and basePackages we tell spring where to 
+//look for classes annotated with stereotype annotations.
 @ComponentScan(basePackages = "entity")
 public class ProjectConfig {
 
