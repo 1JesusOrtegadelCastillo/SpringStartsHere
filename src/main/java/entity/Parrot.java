655ae9abd930ee25f69ffa77b92dc62639fbe97f
JesusOrtega <jesusortegadc@gmail.com>
2022-04-22 22:39:21 -0700

    added no-args constructor to test how many times a Parrot is created

--- src/main/java/entity/Parrot.java
+++ src/main/java/entity/Parrot.java
@@ -4,6 +4,10 @@ public class Parrot {
 
     private String name;
 
+    public Parrot(){
+        System.out.println("Parrot created"); // <- no-args constructor to test
+    }
+
     public String getName(){
         return name;
     }
