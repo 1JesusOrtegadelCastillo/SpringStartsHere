c614e98f89c1acffd7ba87e35d7f0028f9c7097f
JesusOrtega <jesusortegadc@gmail.com>
2022-04-24 01:57:00 -0700

    Example of Qualifier annotation

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -2,6 +2,7 @@ package config;
 
 import entity.Parrot;
 import entity.Person;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,24 +11,25 @@ import org.springframework.context.annotation.Configuration;
 public class ProjectConfig {
 
     @Bean
-    public Parrot parrot1(){ // instance of Parrot 1
+    public Parrot parrot1(){ // instance parrot 1
         Parrot p = new Parrot();
         p.setName("Jerry");
         return p;
     }
 
     @Bean
-    public Parrot parrot2(){ // instance of Parrot 2
+    public Parrot parrot2(){ // instance parrot 2
         Parrot p = new Parrot();
         p.setName("Tom"); // <--- here's Tom
         return p;
     }
 
     @Bean
-    public Person person (Parrot parrot2){ // the name of the parameter matches the name of the bean representing Tom.
+    public Person person (
+            @Qualifier("parrot2") Parrot parrot){ // Using @Qualifier you specify a specific bean from the context.
         Person p = new Person();
         p.setName("Leonidas");
-        p.setParrot(parrot2); 
+        p.setParrot(parrot);
         return p;
     }
 
